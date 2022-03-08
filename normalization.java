c95eb76771e2ad6d9418c2f8595d046429770e97
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2022-03-08 13:14:32 +0800

    Building Skeleton

--- normalization.java
+++ normalization.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class normalization {
-    public static void main(String[] args){
-        // enter a word
-        
+    public static void main(String[] args) {
+        try (// enter a word
+                Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter a word: ");
+            String word = input.nextLine();
+            //call stem and save it to variable
+            String stem = stem(word);
+            //print the word and the stem
+            System.out.println("Word: " + word);
+            System.out.println("Stem: " + stem);
+            
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+    //return the stem word using porter stemmer algorithm without using library
+    public static String stem(String word) {
+        //convert the word to lower case
+        word = word.toLowerCase();
+
+        return word;
     }
 }
